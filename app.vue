6780851d485bfdb8dfd70b5e2266b77ef620e93e
Alois Sečkár <ellrohir@seznam.cz>
2023-01-18 22:21:42 +0100

    smaller screens adjustment

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center">
-        <div class="w-4/5">
+        <div class="mx-2 md:w-4/5">
             <NuxtLayout>
                 <NuxtPage />
             </NuxtLayout>
