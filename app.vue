35dc5258c6930ba1a5cccaee77b19f590e41098c
Alois Sečkár <ellrohir@seznam.cz>
2023-03-18 09:19:47 +0100

    layout not required

--- app.vue
+++ app.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="flex justify-center">
     <div class="mx-2 md:w-4/5">
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
+      <div class="text-center">
+        <header class="mt-1 mb-2">
+          <h1>WBSC Scoring Creator <span class="demo">DEMO</span></h1>
+        </header>
+        <div class="text-justify mb-16">
+          <NuxtPage />
+        </div>
+        <footer class="fixed bottom-0 left-0 w-full p-1 bg-wbsc-blue text-black">
+          Created and maintained by
+          <a href="http://alois-seckar.cz" class="text-white hover:text-yellow-300">Alois Sečkár</a>
+          (2019-2023)
+        </footer>
+      </div>
     </div>
   </div>
 </template>
