ea33231e073620a92a61b74df6bbc72a9707fbf1
Alois Sečkár <ellrohir@seznam.cz>
2023-06-03 10:52:56 +0200

    few more lib actions

--- public/json/_lib.ts
+++ public/json/_lib.ts
@@ -1,14 +1,30 @@
 export const libItems = [
     {
-        "file": "ground-out-63",
+        "file": "out-63",
         "name": "Ground out - SS to 1B"
     },
     {
-        "file": "strike-out-swinging",
+        "file": "k-swinging",
         "name": "Strike out - swinging"
     },
     {
-        "file": "single-lf",
-        "name": "Single hit - LF"
+        "file": "1b-lf",
+        "name": "Single to LF"
+    },
+    {
+        "file": "hr-rc-4rbi",
+        "name": "Home run to RC - 4 RBI"
+    },
+    {
+        "file": "sh-54",
+        "name": "Sacrifice hit - 3B to 2B"
+    },
+    {
+        "file": "sf-8",
+        "name": "Sacrifice fly to CF"
+    },
+    {
+        "file": "gdp-643",
+        "name": "Grounded into DP - SS to 2B to 1B"
     }
 ]
