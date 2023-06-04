4ea9d4daf0a12f6816cbe5128eee0098f5ba0dc9
Alois Sečkár <ellrohir@seznam.cz>
2023-06-04 23:36:32 +0200

    some more lib actions

--- public/json/_lib.ts
+++ public/json/_lib.ts
@@ -24,6 +24,30 @@ export const libItems = [
         "name": "Sacrifice fly to CF"
     },
     {
+        "file": "fc-o6",
+        "name": "Fielder's choice - occuped - SS to 2B"
+    },
+    {
+        "file": "fc-64",
+        "name": "Fielder's choice - SS to 2B"
+    },
+    {
+        "file": "error-5",
+        "name": "Error - fielding by 3B"
+    },
+    {
+        "file": "error-6t",
+        "name": "Error - throwing by SS"
+    },
+    {
+        "file": "error-7f",
+        "name": "Error - dropped fly by LF"
+    },
+    {
+        "file": "error-4e3",
+        "name": "Error - fielding by 1B, assited by 2B"
+    },
+    {
         "file": "gdp-643",
         "name": "Grounded into DP - SS to 2B to 1B"
     }
