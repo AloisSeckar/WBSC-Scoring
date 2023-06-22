8b68fd329f23b2958af4950d0702d65c0e2cc6b6
Alois Sečkár <ellrohir@seznam.cz>
2023-06-22 22:53:26 +0200

    BackstopJS config - naming, etc.

--- backstop_data/engine_scripts/puppet/scenarios/baseAction.js
+++ backstop_data/engine_scripts/puppet/scenarios/baseAction.js
@@ -1,5 +1,8 @@
+// BackstopJS test scenario 03
 module.exports = async (page) => {
+  // pick "base action = Strike out"
   await page.waitForSelector('#input-b-base-action')
   await page.select('#input-b-base-action', 'StrikeOut')
+  // generate action
   await page.click('#button-input-generate')
 }
--- backstop_data/engine_scripts/puppet/scenarios/import.js
+++ backstop_data/engine_scripts/puppet/scenarios/import.js
@@ -1,8 +1,12 @@
+// BackstopJS test scenario 04
 module.exports = async (page) => {
+  // import from library
   await page.waitForSelector('#button-input-import-lib')
   await page.click('#button-input-import-lib')
+  // pick "Out 6-3"
   await page.waitForSelector('#lib-items')
   await page.select('#lib-items', 'out-63.json')
+  // select (and wait for generate action)
   await page.waitForSelector('#lib-select')
   await page.click('#lib-select')
   await page.waitForTimeout(1000)
