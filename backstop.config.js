e6030e397281b84f2ae4ea6c3703b5a77e1a617f
Alois Sečkár <ellrohir@seznam.cz>
2023-07-01 16:04:57 +0200

    BackstopJS - load import scenarios from file

--- backstop.config.js
+++ backstop.config.js
@@ -26,18 +26,10 @@ function getImportScenarioCfg () {
   }
 }
 
-const actions = [
-  'out-63', 'out-54b', 'out-f4', 'out-ff5', 'out-ff2b', 'ks', 'kl', 'ks-2', 'kl-23',
-  'ksb', 'ksr', 'ksi', 'ks-wp', 'ks-e2t', 'ks-2e3', 'kso', 'ksfc', 'kl-pb', '1b-lf', '1b-1b', '2b-ll',
-  '2b-8gr', '3b-rc', 'hr-8', 'hr-8ue', 'hr-8tu', 'hr-rl-i', 'hr-rc-4rbi', 'hr-rc-4rbi', 'sh-54', 'sf-8',
-  'fc-o6', 'fc-64', 'error-5', 'error-6t', 'error-7f', 'error-4e3', '1b-9-e9', '1b-9-e9t', 'error-sh-e5t',
-  'error-sh-5e4', 'error-sf-e8', 'gdp-643', 'gdp-64e3', 'out-if6', 'sb-single', 'sb-double', 'sb-e2t',
-  'cs-26', 'cs-25-o', 'cs-2e6', 'a-13', 'lt'
-]
-
 function getImportScenarios () {
   const scenarios = []
-  actions.forEach(a => scenarios.push({ label: 'action-' + a, ...getImportScenarioCfg() }))
+  const library = require('./public/json/_library.json')
+  library.forEach(json => scenarios.push({ label: 'action-' + json.file, ...getImportScenarioCfg() }))
   return scenarios
 }
 
