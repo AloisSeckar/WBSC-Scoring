e2a873ab55378928b4e576e297f5c5f4f053875d
Alois Sečkár <ellrohir@seznam.cz>
2023-07-03 22:49:05 +0200

    responsivity fixes in import window

--- components/ImportModal.vue
+++ components/ImportModal.vue
@@ -31,9 +31,10 @@ import libItems from '@/assets/json/library.json'
 
 const libFileId = 'lib-items'
 
-const divMain = 'mt-[10%] px-4 max-[500px]:w-[90%] max-[720px]:w-[75%] w-1/2 xl:w-1/3 h-fit border border-black rounded bg-blue-200'
+const divMain = 'mt-[10%] px-4 max-[500px]:w-[90%] max-[1000px]:w-[75%] w-1/2 min-[1400px]:w-1/3 h-fit ' +
+                'border border-black rounded bg-blue-200'
 const divHeader = 'py-4 text-3xl font-bold'
-const divSelect = 'max-[420px]:max-w-[90%]'
+const divSelect = 'max-[650px]:max-w-[90%]'
 const divButton = 'mx-2 my-4 p-2 w-24 inline-block border border-black rounded bg-wbsc-blue hover:bg-sky-300 ' +
                   'text-white hover:text-gray-700 font-bold cursor-pointer'
 
