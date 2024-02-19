50a93a5cd1d6d42a8f372a7f8b721d2790fa8a32
Alois Sečkár <ellrohir@seznam.cz>
2024-02-19 20:43:09 +0100

    feat: version info in footer

---
+++ app.config.ts
@@ -0,0 +1,3 @@
+export default defineAppConfig({
+  version: '1.0.0'
+})
--- app.vue
+++ app.vue
@@ -10,12 +10,14 @@
           <div class="text-justify mb-16">
             <NuxtPage />
           </div>
-          <footer class="fixed bottom-0 left-0 w-full p-1 bg-wbsc-blue text-black">
+          <footer class="fixed bottom-0 left-0 w-full p-1 bg-wbsc-blue text-black text-xs">
             {{ $t('footer.creator') }}
             <NuxtLink to="http://alois-seckar.cz" class="text-white hover:text-yellow-300">
               Alois Sečkár
             </NuxtLink>
             (2019-{{ new Date().getFullYear() }})
+            {{ $t('footer.version') }}
+            <strong>{{ useAppConfig().version }}</strong>
           </footer>
         </div>
       </div>
