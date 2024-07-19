be4b3127052a20b89eb22f620d047889fe271496
Alois Sečkár <ellrohir@seznam.cz>
2024-07-19 19:06:51 +0200

    fix: make conent client-only to allow ssr translations

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <ClientOnly>
     <div id="top" class="flex justify-center">
       <div class="mx-2 md:w-4/5">
         <LanguageSelector />
@@ -24,7 +24,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
