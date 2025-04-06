816fddd5b16b625b2cb85db217f0f0b6c03a5b75
Alois Sečkár <ellrohir@seznam.cz>
2025-04-06 17:16:59 +0200

    fix: update to nuxt content v3

--- components/ContentWrapper.vue
+++ components/ContentWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <ContentRendererMarkdown :value="data || {}" />
+  <ContentRenderer :value="data || {}" />
 </template>
 
 <script setup lang="ts">
@@ -8,7 +8,7 @@ const props = defineProps({
 })
 
 const { locale } = useI18n()
-const { data, refresh } = await useAsyncData(`${props.source}-content`, () => queryContent(`/${locale.value}/${props.source}`).findOne())
+const { data, refresh } = await useAsyncData(`${props.source}-content`, () => queryCollection('content').path(`/${locale.value}/${props.source}`).first())
 watch (() => locale.value, () => {
   refresh()
 })
