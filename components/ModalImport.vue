fdc43062dd998cc7d0753375a43a43fa5a9f942f
Alois Sečkár <ellrohir@seznam.cz>
2025-07-27 09:10:49 +0200

    fix: library action select bg color

--- components/ModalImport.vue
+++ components/ModalImport.vue
@@ -37,7 +37,7 @@ const libFileId = 'lib-items'
 const divMain = 'mt-[10%] px-4 max-[500px]:w-[90%] max-[1200px]:w-[80%] w-1/2 min-[1600px]:w-1/3 h-fit '
   + 'border border-black rounded bg-blue-200'
 const divHeader = 'py-4 text-3xl font-bold'
-const divSelect = 'max-[650px]:max-w-[90%]'
+const divSelect = 'max-[650px]:max-w-[90%] bg-white'
 const divButton = 'mx-2 my-4 p-2 w-28 inline-block border border-black rounded bg-wbsc-blue hover:bg-sky-300 '
   + 'text-white hover:text-gray-700 font-bold cursor-pointer'
 
