ee72c4c8b565ba6cd1aa7c24bfaff4d0322d6a65
AloisSeckar <alois.seckar@gmail.com>
2026-07-28 10:08:18 +0200

    fix: workaround for broken auto-imports in Nuxt 4.5.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
     'nuxt-ignis',
   ],
 
+  // temporary fix for https://github.com/nuxt/nuxt/issues/35858
+  imports: {
+    parser: 'regex',
+  },
+
   devtools: {
     enabled: false,
   },
