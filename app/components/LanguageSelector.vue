d8900fe3c66f89a650132345c8dbfb8b9f28f7db
Alois Sečkár <ellrohir@seznam.cz>
2026-08-29 22:53:48 +0200

    fix: add aria labels to lang selectors

--- app/components/LanguageSelector.vue
+++ app/components/LanguageSelector.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="text-center">
-    <button id="lang-en" class="cursor-pointer" @click="setNewLocale('en')">
+    <button
+      id="lang-en" type="button" class="cursor-pointer"
+      aria-label="English" @click="setNewLocale('en')">
       <img
-        src="/img/flag-en.png" alt="EN"
+        src="/img/flag-en.png" alt=""
         title="English" :class="locale === 'en' ? selectedLang : lang">
     </button>
-    <button id="lang-cs" class="cursor-pointer" @click="setNewLocale('cs')">
+    <button
+      id="lang-cs" type="button" class="cursor-pointer"
+      aria-label="Čeština" @click="setNewLocale('cs')">
       <img
-        src="/img/flag-cs.png" alt="CS"
+        src="/img/flag-cs.png" alt=""
         title="Čeština" :class="locale === 'cs' ? selectedLang : lang">
     </button>
-    <button id="lang-it" class="cursor-pointer" @click="setNewLocale('it')">
+    <button
+      id="lang-it" type="button" class="cursor-pointer"
+      aria-label="Italiano" @click="setNewLocale('it')">
       <img
-        src="/img/flag-it.png" alt="IT"
+        src="/img/flag-it.png" alt=""
         title="Italiano" :class="locale === 'it' ? selectedLang : lang">
     </button>
   </div>
